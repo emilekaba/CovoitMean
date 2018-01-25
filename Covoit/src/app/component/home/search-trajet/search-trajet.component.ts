3968c2133c0fd6881f77e5b59dd0f70a8d3f933a
emilekaba <35562419+emilekaba@users.noreply.github.com>
2018-01-25 22:08:51 +0100

    Add files via upload

---
+++ Covoit/src/app/component/home/search-trajet/search-trajet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-trajet',
+  templateUrl: './search-trajet.component.html',
+  styleUrls: ['./search-trajet.component.css']
+})
+export class SearchTrajetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
