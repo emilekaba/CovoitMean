1ade96336dee206038f971150a3057472f0ac090
emilekaba <35562419+emilekaba@users.noreply.github.com>
2018-01-25 22:07:41 +0100

    Add files via upload

---
+++ Covoit/src/app/component/home/form-dep/form-dep.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-dep',
+  templateUrl: './form-dep.component.html',
+  styleUrls: ['./form-dep.component.css']
+})
+export class FormDepComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
