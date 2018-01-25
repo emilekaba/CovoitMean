e157ee4a1d3784e00db2e1948afcad2351df4f1b
emilekaba <35562419+emilekaba@users.noreply.github.com>
2018-01-25 22:05:06 +0100

    Add files via upload

---
+++ Covoit/src/app/component/home/form-arr/form-arr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-arr',
+  templateUrl: './form-arr.component.html',
+  styleUrls: ['./form-arr.component.css']
+})
+export class FormArrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
